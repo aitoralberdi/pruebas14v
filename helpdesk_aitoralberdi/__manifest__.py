05b12ba17e05e056d2aad19ddb6a212fa07ec0a0
Aitor Alberdi <aitoralberdi@avanzosc.es>
2023-09-13 08:17:33 +0200

    Primer push

---
+++ helpdesk_aitoralberdi/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright <YEAR(S)> <AEODOO>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Helpdesk Aitor Alberdi",
+    "summary": "Helpdesk aitoralberdi",
+    "version": "14.0.1.0.0",
+    "author": "<AEODOO>, Odoo Community Association (OCA)",
+    "maintainers": ["aitoralberdi"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        #"views/helpdesk_name.xml"
+    ],
+}
